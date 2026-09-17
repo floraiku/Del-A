5f7b76ae0ef7d5f020d84c639fe09fc779fc0f31
floraiku <abodi_elhajj@icloud.com>
2026-09-17 17:35:20 +0200

    Jag la till en while-loop och for-loop som skriver ut listan. Jag la även till totalsumman.

--- Inköpslistan.cs
+++ Inköpslistan.cs
@@ -1,2 +1,10 @@
 List<string> names = new List<string>(); // En lista som kan endast innehålla text.
-List<int> prices = new List<int>(); // En lista som kan endast innehålla heltal.
+List<int> prices = new List<int>(); // En lista som kan endast innehålla heltal 
+while (true)
+{
+    int total = 0;
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}.{names[i]} - { [prices]} kr"); total += prices[i];
+
+    }
