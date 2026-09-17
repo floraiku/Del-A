61be162b40f53fd9594d557a89a4d0d744106418
floraiku <abodi_elhajj@icloud.com>
2026-09-17 18:53:25 +0200

    Jag la till en funktion som hjälper att ta bort vara via nummer, jag la även till att den går att hantera ogilitiga nummer utan att krascha.

--- Inköpslistan.cs
+++ Inköpslistan.cs
@@ -1,14 +1,29 @@
 List<string> names = new List<string>(); // En lista som kan endast innehålla text.
 List<int> prices = new List<int>(); // En lista som kan endast innehålla heltal 
 while (true)
-
-    int total = 0;
-for (int i = 0; i < names.Count; i++)
 {
-    Console.WriteLine($"{i + 1}.{names[i]} - { [prices]} kr"); total += prices[i];
+    int total = 0;
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}.{names[i]} - { [prices]} kr"); total += prices[i];
 
-}
-Console.WriteLine($"Totalt: {totalt} kr");
+    }
+    Console.WriteLine($"Totalt: {totalt} kr");
 
-Console.Write("Vara eller nummer:");
-string? input = Console.ReadLine();
+    Console.Write("Vara eller nummer:");
+    string? input = Console.ReadLine();
+
+    if (int.TryParse(input, out int nummer))
+    {
+        int index = nummer - 1;
+        if (index >= 0 && index < names.Count)
+        {
+            names.RemoveAt(index);
+            prices.RemoveAt(index);
+        }
+        else
+        {
+            Console.WriteLine("Den varan finns inte.");
+        }
+    }
+
